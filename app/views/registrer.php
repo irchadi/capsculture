cf4b154afbbde3197adae3c2670760b532eadbb9
irchadi <84560636+irchadi@users.noreply.github.com>
2025-07-14 18:02:42 +0200

    Structure MVC authentification

---
+++ app/views/registrer.php
@@ -0,0 +1,6 @@
+<form action="/register" method="post">
+  <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+  <input type="email" name="email" placeholder="Email" required>
+  <input type="password" name="password" placeholder="Mot de passe" required>
+  <button type="submit">S'inscrire</button>
+</form>
